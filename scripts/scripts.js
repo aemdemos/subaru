0edd6f9adbdcddb80c2d4101f80a8d99d87df7d4
Divyansh Pratap <dipratap@adobe.com>
2024-04-27 16:34:27 +0530

    fix: fixing header animation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -143,27 +143,19 @@ window.addEventListener('scroll', () => {
       element.classList.add('animate');
     }
   });
-});
 
-window.addEventListener('scroll', () => {
-  const animatedElements = document.querySelectorAll('.animated-element');
-  animatedElements.forEach(element => {
-    const elementTop = element.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
-    if (elementTop < windowHeight * 0.96) {
-      element.classList.add('animate');
-    }
-  });
+  const header = document.querySelector('header .nav-wrapper');
+  if (window.scrollY > 100) {
+    header.classList.add('header-slide-down');
+  }
+  if (window.scrollY === 0) {
+    header.classList.remove('header-slide-down');
+  }
 });
 
 function isInViewport(element) {
   const rect = element.getBoundingClientRect();
-  return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
+  return rect.bottom >= window.scrollY;
 }
 
 function animateElementsInView() {
